7bea442da1608ab895b942c51089d8809671ae1f
gabi4321 <gabrieligomes4321@gmail.com>
2025-05-06 01:04:17 -0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm") version "1.8.10"
+    id("com.android.application") version "8.0.0"
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
